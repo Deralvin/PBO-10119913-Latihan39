61246aecbb3c03f45ac786a17a62d35bb9e83dda
Dilan <ramadhanrizqi21@gmail.com>
2020-11-08 21:21:24 +0700

    Latihan 39

---
+++ src/pbo10119913latihan39/PBO10119913Latihan39.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10119913latihan39;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class PBO10119913Latihan39 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Penentu mahasiswa = new Penentu();
+        Scanner scanner = new Scanner(System.in);
+        int i, jumlahMahasiswa, nilaiTerbesar = 0, nilaiTerkecil;
+        String petugas;
+
+        System.out.println("=====Program Nilai Terbesar dan Terkecil Nilai Mahasiswa======");
+        petugas = mahasiswa.namaPetugas();
+        jumlahMahasiswa = mahasiswa.getBanyak_mahasiswa();
+        int[] nilai = new  int[jumlahMahasiswa];
+        for (i =0; i < jumlahMahasiswa; i++) {
+            System.out.printf("Nilai Mahasiswa ke-%d\t\t\t\t= ",i+1);
+            nilai[i] = scanner.nextInt();
+        }
+        System.out.println();
+        System.out.println("======Hasil Nilai Mahasiswa======");
+        nilaiTerbesar = mahasiswa.nilaiTerbesar(nilai);
+        nilaiTerkecil = mahasiswa.nilaiTerkecil(nilai);
+        System.out.println();
+        System.out.println("Nilai Terbesar Adalah "+nilaiTerbesar);
+        System.out.println("Nilai Terkecil Adalah "+nilaiTerkecil);
+        System.out.println();
+        System.out.print("Petugas : "+petugas);
+
+    }
+    
+}
